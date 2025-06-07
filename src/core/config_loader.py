76b46aee88157a11878f37d741d687e6c474cde2
marcory-hub <marcory-hub@users.noreply.github.com>
2025-06-07 08:54:31 +0200

    Make module for validate rquired keys and validate value types

--- src/core/config_loader.py
+++ src/core/config_loader.py
@@ -4,6 +4,21 @@ import logging
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
+def validate_required_keys(config: dict, required_keys: list) -> None:
+    missing_keys = [key for key in required_keys if key not in config]
+    if missing_keys:
+        logging.error(f"Error: Missing required configuration keys: {', '.join(missing_keys)}")
+        raise KeyError(f"Error: Missing required configuration keys: {', '.join(missing_keys)}")
+
+def validate_value_types(config: dict) -> None:
+    if not isinstance(config['conf_threshold'], (int, float)) or not 0 <= config['conf_threshold'] <= 1:
+        logging.error("Error: 'conf_threshold' must be a number between 0 and 1")
+        raise ValueError("Error: 'conf_threshold' must be a number between 0 and 1")
+    
+    if not isinstance(config['capture_interval'], (int, float)) or config['capture_interval'] <= 0:
+        logging.error("Error: 'capture_interval' must be a positive number")
+        raise ValueError("Error: 'capture_interval' must be a positive number")
+
 def load_config(config_path='config/config.yaml') -> dict:
     """Load the configuration from the specified YAML file.
 
@@ -38,21 +53,8 @@ def load_config(config_path='config/config.yaml') -> dict:
         'capture_interval'
     ]
 
-    # Validate all required keys are present
-    missing_keys = [key for key in required_keys if key not in config]
-    if missing_keys:
-        logging.error(f"Error: Missing required configuration keys: {', '.join(missing_keys)}")
-        raise KeyError(f"Error: Missing required configuration keys: {', '.join(missing_keys)}")
-
-    # Validate specific value types and ranges
-    if not isinstance(config['conf_threshold'], (int, float)) or not 0 <= config['conf_threshold'] <= 1:
-        logging.error("Error: 'conf_threshold' must be a number between 0 and 1")
-        raise ValueError("Error: 'conf_threshold' must be a number between 0 and 1")
-    
- 
-    if not isinstance(config['capture_interval'], (int, float)) or config['capture_interval'] <= 0:
-        logging.error("Error: 'capture_interval' must be a positive number")
-        raise ValueError("Error: 'capture_interval' must be a positive number")
+    validate_required_keys(config, required_keys)
+    validate_value_types(config)
 
     return config
 
